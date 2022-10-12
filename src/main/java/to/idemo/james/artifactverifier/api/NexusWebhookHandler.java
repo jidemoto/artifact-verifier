eaf1b455412913cadacbdffe2e8e11d5b9146d45
James Idemoto <james.idemoto@gmail.com>
2022-10-11 22:46:00 -0700

    Adds swagger documentation annotations

--- src/main/java/to/idemo/james/artifactverifier/api/NexusWebhookHandler.java
+++ src/main/java/to/idemo/james/artifactverifier/api/NexusWebhookHandler.java
@@ -1,5 +1,9 @@
 package to.idemo.james.artifactverifier.api;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -20,17 +24,29 @@ public class NexusWebhookHandler {
         this.verifierService = verifierService;
     }
 
+    @Operation(
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "The Component or Asset depending on the Nexus Webhook Type",
+                    required = true,
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(oneOf = {NexusComponent.class, NexusAsset.class, Object.class})),
+                    }
+            )
+    )
     @PostMapping(headers = "X-Nexus-Webhook-Id=rm:repository:component")
     public void handleComponentNotification(@RequestBody NexusComponent component) {
         logger.info("Received component: {}", component);
 
     }
 
+    @Hidden
     @PostMapping(headers = "X-Nexus-Webhook-Id=rm:repository:asset")
     public void handleAssetNotification(@RequestBody NexusAsset component) {
         logger.info("Received Asset: {}", component);
     }
 
+    @Hidden
     @PostMapping
     public void fallbackHandler(@RequestHeader("X-Nexus-Webhook-Id") String webhookId,
                                 @RequestBody Map<String, Object> payload) {
