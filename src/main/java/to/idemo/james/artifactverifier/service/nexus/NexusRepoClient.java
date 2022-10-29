5d7022f6592a7fda47d4ebc1887f8eb0a690e051
James Idemoto <james.idemoto@gmail.com>
2022-10-29 15:47:23 -0700

    Adds comments

--- src/main/java/to/idemo/james/artifactverifier/service/nexus/NexusRepoClient.java
+++ src/main/java/to/idemo/james/artifactverifier/service/nexus/NexusRepoClient.java
@@ -1,5 +1,10 @@
 package to.idemo.james.artifactverifier.service.nexus;
 
+/**
+ * Interface for Nexus repo clients (specific format handlers e.g. npm)
+ *
+ * Exceptions will be thrown internally if more than one client exists per format
+ */
 public interface NexusRepoClient extends RepositoryClient {
     String handlesFormat();
 }
