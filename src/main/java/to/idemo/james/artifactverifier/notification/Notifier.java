db6dd13776d3796091532c1c40a97d9e41a06816
James Idemoto <james.idemoto@gmail.com>
2022-10-29 15:48:04 -0700

    Externalizes Notifier interface to another project

--- src/main/java/to/idemo/james/artifactverifier/notification/Notifier.java
+++
@@ -1,6 +0,0 @@
-package to.idemo.james.artifactverifier.notification;
-
-public interface Notifier {
-    void warn(String artifactName, String artifactLocation, String attributedUser, String reason);
-    void alert(String artifactName, String artifactType, String artifactLocation, String attributedUser, String reason);
-}
